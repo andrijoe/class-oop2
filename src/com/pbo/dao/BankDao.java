37a08e6071243424f49d0a2685992e02874f7740
andrie <andrie@Rhinoceros>
2019-10-14 21:39:31 +0700

    crud services

--- src/com/pbo/dao/BankDao.java
+++ src/com/pbo/dao/BankDao.java
@@ -36,4 +36,78 @@ public class BankDao {
 		
 		return listBank;
 	}
+	
+	public Bank getBankById(String id) {
+		Bank bank = null;
+		try {
+			Connection conn = DbConnection.getConnection();
+			String query = "select * from mbank where id = ?";
+			PreparedStatement ps = conn.prepareStatement(query);
+			ps.setInt(1, Integer.parseInt(id));
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				bank = new Bank();
+				bank.setAddress(rs.getString("address"));
+				bank.setId(rs.getLong("id"));
+				bank.setIsDelete(rs.getString("isdelete"));
+				bank.setName(rs.getString("name"));
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return bank;
+	}
+	
+	public boolean deleteBank(String id) {
+		try {
+			Connection conn = DbConnection.getConnection();
+			String query = "update mbank set isdelete = ? "
+					+ "where id = ?";
+			PreparedStatement ps = conn.prepareStatement(query);
+			ps.setString(1, "1");
+			ps.setInt(2, Integer.parseInt(id));
+			ps.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public boolean saveUpdateBank(Bank bank) {
+		try {
+			Connection conn = DbConnection.getConnection();
+			String query = "insert into mbank "
+					+ "(name, address, isdelete) values "
+					+ "(?, ?, ?)";
+			
+			if (bank.getId() != null) {
+				query = "update mbank set name = ?, "
+						+ "address = ?, isdelete = ? "
+						+ "where id = ?";
+			}
+			
+			PreparedStatement ps = conn.prepareStatement(query);
+			ps.setString(1, bank.getName());
+			ps.setString(2, bank.getAddress());
+			ps.setString(3, "0");
+			
+			if (bank.getId() != null) {
+				ps.setLong(4, bank.getId());
+			}
+			
+			ps.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
+	}
+	
 }
