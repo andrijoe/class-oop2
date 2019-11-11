8e33b50cbae910f2671f352bcc688ae4e7c65c6a
Who Are You <Who Are You@Welcome>
2019-11-12 00:02:16 +0700

    LeasingCostumer-aji ver 1.0

--- src/com/pbo/bean/LeasingCostumer.java
+++ src/com/pbo/bean/LeasingCostumer.java
@@ -2,4 +2,53 @@ package com.pbo.bean;
 
 public class LeasingCostumer {
 	
+	private Long id;
+	private String No_KTP;
+	private String Nama;
+	private String Jenkel;
+	private String TTL;
+	private String Alamat;
+	private String Phone;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNo_KTP() {
+		return No_KTP;
+	}
+	public void setNo_KTP(String No_KTP) {
+		this.No_KTP = No_KTP;
+	}
+	public String getNama() {
+		return Nama;
+	}
+	public void setNama(String Nama) {
+		this.Nama = Nama;
+	}
+	public String getJenkel() {
+		return Jenkel;
+	}
+	public void setJenkel(String Jenkel) {
+		this.Jenkel = Jenkel;
+	}
+	public String getTTL() {
+		return TTL;
+	}
+	public void setTTL(String TTL) {
+		this.TTL = TTL;
+	}
+	public String getAlamat() {
+		return Alamat;
+	}
+	public void setAlamat(String Alamat) {
+		this.Alamat = Alamat;
+	}
+	public String getPhone() {
+		return Phone;
+	}
+	public void setPhone(String Phone) {
+		this.Phone = Phone;
+	}
 }
