1e037c7e32f68dcda27ec6cfd53f0acd79ae2301
Husnul Firdaus <Husnul Firdaus@HusnulFirdaus>
2019-11-11 21:39:45 +0700

    KTA - iwa dan andy versi 1.0

---
+++ src/com/pbo/bean/CustomerKta.java
@@ -0,0 +1,57 @@
+package com.pbo.bean;
+
+public class CustomerKta {
+
+	private Long Id;
+	private String Name;
+	private String Address;
+	private String Jobs;
+	private String KTP_Number;
+	private String DateOfBirth;
+	private String Gender;
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public String getJobs() {
+		return Jobs;
+	}
+	public void setJobs(String jobs) {
+		Jobs = jobs;
+	}
+	public String getKTP_Number() {
+		return KTP_Number;
+	}
+	public void setKTP_Number(String kTP_Number) {
+		KTP_Number = kTP_Number;
+	}
+	public String getDateOfBirth() {
+		return DateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		DateOfBirth = dateOfBirth;
+	}
+	public String getGender() {
+		return Gender;
+	}
+	public void setGender(String gender) {
+		Gender = gender;
+	}
+	
+	
+	
+}
