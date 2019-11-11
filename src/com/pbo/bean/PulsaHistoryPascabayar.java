191e15a60aaa2c2e3ce28d7c52de822bfb0784ea
Ilmi <Ilmi@DESKTOP-22MCGUA>
2019-11-11 21:46:19 +0700

    PULSA - ADE versi 1.0

---
+++ src/com/pbo/bean/PulsaHistoryPascabayar.java
@@ -0,0 +1,29 @@
+package com.pbo.bean;
+
+public class PulsaHistoryPascabayar {
+	private Long id;
+	private String idtrxpascabayar;
+	private String tglpembayaran;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getIdtrxpascabayar() {
+		return idtrxpascabayar;
+	}
+	public void setIdtrxpascabayar(String idtrxpascabayar) {
+		this.idtrxpascabayar = idtrxpascabayar;
+	}
+	public String getTglpembayaran() {
+		return tglpembayaran;
+	}
+	public void setTglpembayaran(String tglpembayaran) {
+		this.tglpembayaran = tglpembayaran;
+	}
+	
+	
+
+}
