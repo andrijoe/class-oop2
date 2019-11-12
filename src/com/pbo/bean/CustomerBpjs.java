9d602ff7c863c1b841027cf77410c99ce8c6f2d4
renaldy18 <renaldy18@renaldy18>
2019-11-12 17:06:07 +0700

    # WARNING: head commit changed in the meantime
    BPJS RENALDY OKTAVIANO v1.0

---
+++ src/com/pbo/bean/CustomerBpjs.java
@@ -0,0 +1,49 @@
+package com.pbo.bean;
+
+public class CustomerBpjs {
+
+	private long id;
+	private long number;
+	private String name;
+	private String address;
+	private long phone;
+	private String email;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public long getNumber() {
+		return number;
+	}
+	public void setNumber(long number) {
+		this.number = number;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
