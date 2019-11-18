e3d7ca61935f75a20a28c31ab80052aba449dd0b
muslimin <muslimin@visionet.co.id>
2019-11-18 11:59:13 +0700

    Leasing v1 Muslimin

---
+++ src/com/pbo/bean/Leasing.java
@@ -0,0 +1,47 @@
+package com.pbo.bean;
+
+public class Leasing {
+private Long id;
+private String nama_leasing;
+private String alamat;
+private String tipe;
+private Number phone;
+private String email;
+public Long getId() {
+	return id;
+}
+public void setId(Long id) {
+	this.id = id;
+}
+public String getNama_leasing() {
+	return nama_leasing;
+}
+public void setNama_leasing(String nama_leasing) {
+	this.nama_leasing = nama_leasing;
+}
+public String getAlamat() {
+	return alamat;
+}
+public void setAlamat(String alamat) {
+	this.alamat = alamat;
+}
+public String getTipe() {
+	return tipe;
+}
+public void setTipe(String tipe) {
+	this.tipe = tipe;
+}
+public Number getPhone() {
+	return phone;
+}
+public void setPhone(Number phone) {
+	this.phone = phone;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+
+}
