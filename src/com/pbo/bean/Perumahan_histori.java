77722d8c22dc9cedb7bcec174c4afe7b77d28771
B 21 <B 21@B21-PC>
2019-11-18 20:24:47 +0700

    Ghofur rahmat

---
+++ src/com/pbo/bean/Perumahan_histori.java
@@ -0,0 +1,41 @@
+package com.pbo.bean;
+
+public class Perumahan_histori {
+	private Long id;
+	private String cicilan;
+	private String nominal;
+	private String denda;
+	private String tglcicil;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getCicilan() {
+		return cicilan;
+	}
+	public void setCicilan(String cicilan) {
+		this.cicilan = cicilan;
+	}
+	public String getNominal() {
+		return nominal;
+	}
+	public void setNominal(String nominal) {
+		this.nominal = nominal;
+	}
+	public String getDenda() {
+		return denda;
+	}
+	public void setDenda(String denda) {
+		this.denda = denda;
+	}
+	public String getTglcicil() {
+		return tglcicil;
+	}
+	public void setTglcicil(String tglcicil) {
+		this.tglcicil = tglcicil;
+	}
+
+
+}
