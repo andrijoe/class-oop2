e0c1019731cb66dafb2b2f5d8b380d12f69e07cb
B 12 <B 12@B12-PC>
2019-11-18 21:30:33 +0700

    Transportasi Customer (Dheanisa)

---
+++ src/com/pbo/bean/TransportasiCustomer.java
@@ -0,0 +1,47 @@
+package com.pbo.bean;
+
+public class TransportasiCustomer {
+
+	private Long id;
+	private Long no_ktp;
+	private String name;
+	private String address;
+	private Long phone;
+	private String email;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getNo_ktp() {
+		return no_ktp;
+	}
+	public void setNo_ktp(Long no_ktp) {
+		this.no_ktp = no_ktp;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Long getPhone() {
+		return phone;
+	}
+	public void setPhone(Long phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
