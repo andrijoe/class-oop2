0367317b0931983cdfaea036b70ba4c6d4541a07
muslimin <muslimin@visionet.co.id>
2019-11-18 21:46:19 +0700

    muslimin

---
+++ src/com/pbo/bean/LeasingVehicle.java
@@ -0,0 +1,33 @@
+package com.pbo.bean;
+
+public class LeasingVehicle {
+private Long id;
+private String nama;
+private String desc;
+private String Vendor;
+public Long getId() {
+	return id;
+}
+public void setId(Long id) {
+	this.id = id;
+}
+public String getNama() {
+	return nama;
+}
+public void setNama(String nama) {
+	this.nama = nama;
+}
+public String getDesc() {
+	return desc;
+}
+public void setDesc(String desc) {
+	this.desc = desc;
+}
+public String getVendor() {
+	return Vendor;
+}
+public void setVendor(String vendor) {
+	Vendor = vendor;
+}
+
+}
