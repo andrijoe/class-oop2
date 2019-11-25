96fc43b3df19df0f7e04376d65c9ff378ccc9559
Husnul Firdaus <Husnul Firdaus@HusnulFirdaus>
2019-11-25 20:41:37 +0700

    KTA Andy, Iwa + Crack

--- src/com/pbo/dao/KtaBankkDao.java
+++ src/com/pbo/dao/KtaBankkDao.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-
+import com.pbo.bean.Bank;
 import com.pbo.bean.KtaBankk;
 import com.pbo.util.DbConnection;
 
@@ -38,4 +38,83 @@ public class KtaBankkDao {
 		
 		return listKtaBankk;
 	}
+public KtaBankk getBankById(String id) {
+		KtaBankk bankk = null;
+		try {
+			Connection conn = DbConnection.getConnection();
+			String query = "select * from mbank where id = ?";
+			PreparedStatement ps = conn.prepareStatement(query);
+			ps.setInt(1, Integer.parseInt(id));
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				KtaBankk Bankk = new KtaBankk();
+				Bankk.setId(rs.getLong("id"));
+				Bankk.setBank_name(rs.getString("bank_name"));
+				Bankk.setAddress(rs.getString("address"));
+				Bankk.setPhone(rs.getString("phone"));
+				Bankk.setEmail(rs.getString("email"));
+
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return bankk;
+	}
+	
+	public boolean deleteBank(String id) {
+		try {
+			Connection conn = DbConnection.getConnection();
+			String query = "update mbank set isdelete = ? "
+					+ "where id = ?";
+			PreparedStatement ps = conn.prepareStatement(query);
+			ps.setString(1, "1");
+			ps.setInt(2, Integer.parseInt(id));
+			ps.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public boolean saveUpdateBank(KtaBankk bankk) {
+		try {
+			Connection conn = DbConnection.getConnection();
+			String query = "insert into mbank "
+					+ "(name, address, isdelete) values "
+					+ "(?, ?, ?)";
+			
+			if (bankk.getId() != null) {
+				query = "update mbank set name = ?, "
+						+ "address = ?, isdelete = ? "
+						+ "where id = ?";
+			}
+			
+			PreparedStatement ps = conn.prepareStatement(query);
+			ps.setString(1, bankk.getBank_name());
+			ps.setString(2, bankk.getAddress());
+			ps.setString(3, bankk.getPhone());
+			ps.setString(4, bankk.getEmail());
+			ps.setString(5, "0");
+			
+			if (bankk.getId() != null) {
+				ps.setLong(4, bankk.getId());
+			}
+			
+			ps.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
+	}
+	
 }
+
+
--- src/com/pbo/dao/KtaCustomerrDao.java
+++ src/com/pbo/dao/KtaCustomerrDao.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.pbo.bean.KtaBankk;
 import com.pbo.bean.KtaCustomerr;
 import com.pbo.util.DbConnection;
 
@@ -40,4 +41,60 @@ public class KtaCustomerrDao {
 		
 		return listBank;
 	}
+	public boolean deleteBank(String id) {
+		try {
+			Connection conn = DbConnection.getConnection();
+			String query = "update mbank set isdelete = ? "
+					+ "where id = ?";
+			PreparedStatement ps = conn.prepareStatement(query);
+			ps.setString(1, "1");
+			ps.setInt(2, Integer.parseInt(id));
+			ps.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public boolean saveUpdateBank(KtaCustomerr Ktacust) {
+		try {
+			Connection conn = DbConnection.getConnection();
+			String query = "insert into mbank "
+					+ "(name, address, isdelete) values "
+					+ "(?, ?, ?)";
+			
+			if (Ktacust.getId_cust() != null) {
+				query = "update mbank set name = ?, "
+						+ "address = ?, isdelete = ? "
+						+ "where id = ?";
+			}
+			
+			PreparedStatement ps = conn.prepareStatement(query);
+			ps.setString(1, Ktacust.getName());
+			ps.setString(2, Ktacust.getAddress());
+			ps.setString(3, Ktacust.getJobs());
+			ps.setString(4, Ktacust.getKTP_Number());
+			ps.setString(5, Ktacust.getDateOfBirth());
+			ps.setString(6, Ktacust.getGender());
+			ps.setString(7, "0");
+			
+			if (Ktacust.getId_cust() != null) {
+				ps.setLong(4, Ktacust.getId_cust());
+			}
+			
+			ps.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
+	}
+	
 }
+
+
