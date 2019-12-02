d6097792827af9ca40bdc6e53bc2cad1ff3d93d2
LENOVO <LENOVO@DESKTOP-6ST7UQK>
2019-12-02 20:52:25 +0700

    Raihan Ilham Alamsyah

---
+++ src/com/pbo/bean/Pln_PascaBayar.java
@@ -0,0 +1,42 @@
+package com.pbo.bean;
+
+public class Pln_PascaBayar {
+
+	private Long id;
+	private String No_Meter;
+	private String Nominal_Tagihan;
+	private String Tgl_Pembayaran;
+	private String No_Bukti_Transaksi;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNo_Meter() {
+		return No_Meter;
+	}
+	public void setNo_Meter(String no_Meter) {
+		No_Meter = no_Meter;
+	}
+	public String getNominal_Tagihan() {
+		return Nominal_Tagihan;
+	}
+	public void setNominal_Tagihan(String nominal_Tagihan) {
+		Nominal_Tagihan = nominal_Tagihan;
+	}
+	public String getTgl_Pembayaran() {
+		return Tgl_Pembayaran;
+	}
+	public void setTgl_Pembayaran(String tgl_Pembayaran) {
+		Tgl_Pembayaran = tgl_Pembayaran;
+	}
+	public String getNo_Bukti_Transaksi() {
+		return No_Bukti_Transaksi;
+	}
+	public void setNo_Bukti_Transaksi(String no_Bukti_Transaksi) {
+		No_Bukti_Transaksi = no_Bukti_Transaksi;
+	}
+	
+	
+}
