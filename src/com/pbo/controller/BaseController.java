ef89223e2f0a407fc54e11d0db73f93a1cebbc7e
pandri.pandri <pandri.pandri@CRJYCX2.wirecard.lan>
2020-01-03 17:59:18 +0700

    bug fixing

--- src/com/pbo/controller/BaseController.java
+++ src/com/pbo/controller/BaseController.java
@@ -1,28 +1,28 @@
 package com.pbo.controller;
 
+import java.awt.Menu;
 import java.util.List;
+
+import javax.persistence.Access;
 
 import org.springframework.web.servlet.ModelAndView;
 
-import com.ngalongware.bean.Access;
-import com.ngalongware.bean.UserGenerate;
-import com.ngalongware.type.Menu;
-import com.ngalongware.util.Connection;
-import com.ngalongware.util.SessionManager;
+import com.pbo.util.SessionManager;
 
 public class BaseController {
 	
 	public static ModelAndView model () {
 		SessionManager sessionManager = SessionManager.getInstance();
 		ModelAndView model = new ModelAndView();
-		Access loggedUser = sessionManager.getLoggedUser();
-		
-		if (loggedUser != null) {
-			List<Menu> listMenu = sessionManager.getListByAccess(loggedUser.getAccessType());
-			
-			model.addObject("currentUser", loggedUser);
-			model.addObject("listMenu", listMenu);
-		}
+		/*
+		 * Access loggedUser = sessionManager.getLoggedUser();
+		 * 
+		 * if (loggedUser != null) { List<Menu> listMenu =
+		 * sessionManager.getListByAccess(loggedUser.getAccessType());
+		 * 
+		 * model.addObject("currentUser", loggedUser); model.addObject("listMenu",
+		 * listMenu); }
+		 */
 		
 		return model;
 	}
