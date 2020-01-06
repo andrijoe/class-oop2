2a67981e50d42eaca8312a0d41a2941bbd1f0f7b
muhamad jevri <muhamad jevri@LAPTOP-CNRE2C42>
2020-01-06 21:50:58 +0700

    michael-bean

---
+++ src/com/pbo/bean/Ht_Transportasi.java
@@ -0,0 +1,26 @@
+package com.pbo.bean;
+
+public class Ht_Transportasi {
+	 private Long id;
+	    private String no_tiket;
+	    private String tgl_transaksi;
+		public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getNo_tiket() {
+			return no_tiket;
+		}
+		public void setNo_tiket(String no_tiket) {
+			this.no_tiket = no_tiket;
+		}
+		public String getTgl_transaksi() {
+			return tgl_transaksi;
+		}
+		public void setTgl_transaksi(String tgl_transaksi) {
+			this.tgl_transaksi = tgl_transaksi;
+		}
+
+}
