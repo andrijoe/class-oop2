6b19a0adaa2e282b4946d05e1c45fce302d9e0cb
Raihan <Raihan@DESKTOP-41JKBN2>
2020-01-06 20:33:12 +0700

    Raihan-orm

--- src/com/pbo/bean/Pln_PascaBayar.java
+++ src/com/pbo/bean/Pln_PascaBayar.java
@@ -1,40 +1,61 @@
 package com.pbo.bean;
 
-public class Pln_PascaBayar {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="mleasingpln")
+
+public class Pln_PascaBayar {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
 	private Long id;
-	private String No_Meter;
-	private String Nominal_Tagihan;
-	private String Tgl_Pembayaran;
-	private String No_Bukti_Transaksi;
+	
+	@Column(name="No_Meter")
+	private Double No_Meter;
+	
+	@Column(name="Nominal_Tagihan")
+	private Double Nominal_Tagihan;
+	
+	@Column(name="Tgl_Pembayaran")
+	private Double Tgl_Pembayaran;
+	
+	@Column(name="No_Bukti_Transaksi")
+	private Double No_Bukti_Transaksi;
+	
 	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public String getNo_Meter() {
+	public Double getNo_Meter() {
 		return No_Meter;
 	}
-	public void setNo_Meter(String no_Meter) {
+	public void setNo_Meter(Double no_Meter) {
 		No_Meter = no_Meter;
 	}
-	public String getNominal_Tagihan() {
+	public Double getNominal_Tagihan() {
 		return Nominal_Tagihan;
 	}
-	public void setNominal_Tagihan(String nominal_Tagihan) {
+	public void setNominal_Tagihan(Double nominal_Tagihan) {
 		Nominal_Tagihan = nominal_Tagihan;
 	}
-	public String getTgl_Pembayaran() {
+	public Double getTgl_Pembayaran() {
 		return Tgl_Pembayaran;
 	}
-	public void setTgl_Pembayaran(String tgl_Pembayaran) {
+	public void setTgl_Pembayaran(Double tgl_Pembayaran) {
 		Tgl_Pembayaran = tgl_Pembayaran;
 	}
-	public String getNo_Bukti_Transaksi() {
+	public Double getNo_Bukti_Transaksi() {
 		return No_Bukti_Transaksi;
 	}
-	public void setNo_Bukti_Transaksi(String no_Bukti_Transaksi) {
+	public void setNo_Bukti_Transaksi(Double no_Bukti_Transaksi) {
 		No_Bukti_Transaksi = no_Bukti_Transaksi;
 	}
 	
